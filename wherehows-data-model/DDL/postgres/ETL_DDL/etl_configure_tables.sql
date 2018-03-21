fd37d7e86d82c4aa6937c18913f6e0b4d27e54fa
Jon Gherardini <jon.gherardini@ezcater.com>
2018-03-21 15:05:48 -0400

    Adds boilerplate copyright label to Postgres DDLs

--- wherehows-data-model/DDL/postgres/ETL_DDL/etl_configure_tables.sql
+++ wherehows-data-model/DDL/postgres/ETL_DDL/etl_configure_tables.sql
@@ -1,5 +1,6 @@
 --
 -- Copyright 2015 LinkedIn Corp. All rights reserved.
+-- Postgres conversion copyright 2018 ezCater, Inc. All rights reserved.
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
